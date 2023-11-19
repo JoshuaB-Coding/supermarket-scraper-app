7ee227f3214e167be285acaeb1fca8745b980ac5
Joshua Binns <joshuabinns.coding@gmail.com>
2023-11-19 14:53:28 +0000

    add api keys, authorisation handling and a test template

--- scraper/src/v1/index.ts
+++ scraper/src/v1/index.ts
@@ -1,8 +1,25 @@
 import * as express from 'express';
+import * as dotenv from 'dotenv';
 import routes from './routes';
 
+dotenv.config();
+
 const app = express();
-const port = process.env.PORTAL_PORT || 5000;
+const port = process.env.SCRAPER_PORT || 5000;
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, API-Key');
+    next();
+});
+
+app.use((req, res, next) => {
+    const apiKey = req.get('API-Key');
+    if (!apiKey || apiKey !== process.env.SCRAPER_API_KEY) {
+        return res.status(401).json({ error: 'Unauthorised' });
+    }
+    next();
+});
 
 app.use(express.json());
 
